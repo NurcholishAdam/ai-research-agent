e83e2cd5ce23a2513eb54e8821229715d93a98ea
NurcholishAdam <nurcholisadam@gmail.com>
2025-06-13 15:15:20 +0700

    Update vector_store.py

--- memory/vector_store.py
+++ memory/vector_store.py
@@ -1,1 +1,7 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+
+def init_vector_db():
+    embedding = OpenAIEmbeddings()
+    return Chroma(embedding_function=embedding)
 
