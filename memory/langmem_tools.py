96d9cf694bcfa70c9c2403fde7d87cecd7661424
NurcholishAdam <nurcholisadam@gmail.com>
2025-06-13 15:14:53 +0700

    Update langmem_tools.py

--- memory/langmem_tools.py
+++ memory/langmem_tools.py
@@ -1,1 +1,7 @@
+from langmem import create_manage_memory_tool, create_search_memory_tool
+
+def get_memory_tools():
+    manage_tool = create_manage_memory_tool(name="save_memory")
+    search_tool = create_search_memory_tool(name="search_memory")
+    return [manage_tool, search_tool]
 
